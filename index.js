baa5885c06a72e79cad8311cd2a9d68c6ef2485c
erin <erinverkoulen93@gmail.com>
2021-04-28 23:31:54 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(numberOfFeet){
+  let greeting;
+  if (numberOfFeet <= 400) {
+    greeting = "This one is on me!";
+  }else if (numberOfFeet >= 2000 && numberOfFeet < 2500) {
+    greeting = "I will gladly take your thirty bucks.";
+  }else if (numberOfFeet > 2500) {
+    greeting = "No can do.";
+  }
+  return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let response;
+  switch(tip) {
+    case "generous" :
+      response = "Thank you so much.";
+      break;
+    case "not as generous" :
+      response = "Thank you.";
+      break;
+    default: response = "Bye.";  
+  }
+  return response;
+  
 }
